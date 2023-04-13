4bb0f08009767cc9945bf2485f8f20fe335782ff
LeoAraujo30 <leonardo.araujo300301@gmail.com>
2023-04-13 17:01:07 -0300

    requisitos 1 e 2 feitos

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -1,6 +1,40 @@
 from src.models.dish import Dish  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, restriction_map
+import pytest
 
 
 # Req 2
 def test_dish():
-    pass
+    dish1 = Dish("lasanha presunto", 25.90)
+    dish2 = Dish("lasanha berinjela", 27.00)
+
+    assert dish1.name == "lasanha presunto"
+    assert dish1.price == 25.90
+    assert dish1.recipe == {}
+    assert dish1.__repr__() == "Dish('lasanha presunto', R$25.90)"
+    assert dish1.__hash__() == dish1.__hash__()
+    assert dish1.__hash__() != dish2.__hash__()
+    assert dish1.__eq__(dish1) is True
+    assert dish1.__eq__(dish2) is False
+
+    dish1.add_ingredient_dependency(Ingredient("presunto"), 15)
+    dish1.add_ingredient_dependency(Ingredient("queijo mussarela"), 15)
+
+    restriction1_presunto = restriction_map().get("presunto", set())
+    restriction1_queijo = restriction_map().get("queijo mussarela", set())
+
+    assert dish1.get_restrictions() == restriction1_presunto.union(
+        restriction1_queijo
+    )
+    assert dish1.get_ingredients() == {
+        Ingredient('presunto'), Ingredient('queijo mussarela')
+    }
+
+    with pytest.raises(TypeError, match="Dish price must be float."):
+        Dish("lasanha presunto", "dasdas")
+
+    with pytest.raises(
+        ValueError,
+        match="Dish price must be greater then zero."
+    ):
+        Dish("lasanha berinjela", -27.00)
--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,16 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, restriction_map  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    pass
+    ingredient1 = Ingredient("presunto")
+    restriction = restriction_map().get("presunto", set())
+    ingredient2 = Ingredient("frango")
+
+    assert ingredient1.name == "presunto"
+    assert ingredient1.restrictions == restriction
+    assert ingredient1.__hash__() == ingredient1.__hash__()
+    assert ingredient1.__hash__() != ingredient2.__hash__()
+    assert ingredient1.__eq__(ingredient1) is True
+    assert ingredient1.__eq__(ingredient2) is False
+    assert ingredient1.__repr__() == "Ingredient('presunto')"
